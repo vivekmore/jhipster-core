307f9475dd116bffb3b0d04d274094f0ed3d3954
Vivek More <vivz.thats.me@gmail.com>
2017-10-14 08:18:58 -0400

    add JhipsterCoreExceptionType enum

---
+++ lib/exceptions/jhipster-core-exception-type.ts
@@ -0,0 +1,21 @@
+export enum JhipsterCoreExceptionType {
+
+  Assertion = 'Assertion',
+  FileNotFound = 'FileNotFound',
+  IllegalArgument = 'IllegalArgument',
+  IllegalAssociation = 'IllegalAssociation',
+  IllegalName = 'IllegalName',
+  IllegalOption = 'IllegalOption',
+  InvalidObject = 'InvalidObject',
+  MalformedAssociation = 'MalformedAssociation',
+  NoSQLModeling = 'NoSQLModeling',
+  NullPointer = 'NullPointer',
+  UndeclaredEntity = 'UndeclaredEntity',
+  UnsupportedOperation = 'UnsupportedOperation',
+  WrongAssociation = 'WrongAssociation',
+  WrongFile = 'WrongFile',
+  WrongDir = 'WrongDir',
+  WrongType = 'WrongType',
+  WrongValidation = 'WrongValidation'
+
+}
