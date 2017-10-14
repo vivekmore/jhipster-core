c2950572361946ea2ce3adb9d111b8c65f4a3d73
Vivek More <vivz.thats.me@gmail.com>
2017-10-14 13:07:06 -0400

    update test for JhipsterCoreException

--- test/spec/exceptions/jhipster_core_exception_test.ts
+++ test/spec/exceptions/jhipster_core_exception_test.ts
@@ -1,26 +1,24 @@
+import { expect } from 'chai';
+import { JhipsterCoreException } from '../../../lib/exceptions/jhipster_core_exception';
 
-
-/* eslint-disable no-new, no-unused-expressions */
-const expect = require('chai').expect;
-const BuildException = require('../../../lib/exceptions/exception_factory').BuildException;
-
-describe('ExceptionFactory', () => {
-  describe('::BuildException', () => {
+describe('JhipsterCoreException', () => {
+  describe('::constructor', () => {
     it('adds the \'Exception\' suffix to the names', () => {
-      const exception = BuildException('Working', null);
+      const exception = new JhipsterCoreException('Working', null);
       expect(exception.name).to.eq('WorkingException');
     });
     it('builds throwable objects', () => {
       try {
-        throw new BuildException('Working', null);
+        throw new JhipsterCoreException('Working', null);
       } catch (error) {
-        expect(error.name).to.be.defined;
+        expect(error.name).to.eq('WorkingException');
+        expect(error.message).to.be.empty;
       }
     });
     describe('when only passing a name', () => {
       it('takes the name and adds no message', () => {
-        const exception1 = BuildException('Working', null);
-        const exception2 = BuildException('Working', '');
+        const exception1 = new JhipsterCoreException('Working', null);
+        const exception2 = new JhipsterCoreException('Working', '');
         expect(exception1.name).to.eq('WorkingException');
         expect(exception1.message).to.be.empty;
         expect(exception2.name).to.eq('WorkingException');
@@ -29,8 +27,8 @@ describe('ExceptionFactory', () => {
     });
     describe('when only passing a message', () => {
       it('just adds the suffix and keeps the message', () => {
-        const exception1 = BuildException(null, 'The message');
-        const exception2 = BuildException('', 'The message');
+        const exception1 = new JhipsterCoreException(null, 'The message');
+        const exception2 = new JhipsterCoreException('', 'The message');
         expect(exception1.name).to.eq('Exception');
         expect(exception1.message).to.eq('The message');
         expect(exception2.name).to.eq('Exception');
@@ -39,14 +37,14 @@ describe('ExceptionFactory', () => {
     });
     describe('when passing in a name and a message', () => {
       it('keeps both', () => {
-        const exception = BuildException('Good', 'The message');
+        const exception = new JhipsterCoreException('Good', 'The message');
         expect(exception.name).to.eq('GoodException');
         expect(exception.message).to.eq('The message');
       });
     });
     describe('when not passing anything', () => {
       it('names the exception \'Exception\' and puts no message', () => {
-        const exception = BuildException(null, null);
+        const exception = new JhipsterCoreException(null, null);
         expect(exception.name).to.eq('Exception');
         expect(exception.message).to.be.empty;
       });
